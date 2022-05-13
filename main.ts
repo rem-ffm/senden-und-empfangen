a10dcb2d19c33aeb3b1c7ca2199afbc6fde9b68f
rem-ffm <105443658+rem-ffm@users.noreply.github.com>
2022-05-13 14:06:58 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString(Text)
+    basic.clearScreen()
+    Text = ""
+})
+input.onButtonPressed(Button.A, function () {
+    Position += -1
+    if (Position == -1) {
+        Position = 27
+    }
+    Buchstabe = ABC.charAt(Position)
+    basic.showString(Buchstabe)
+})
+input.onButtonPressed(Button.AB, function () {
+    Text += Buchstabe
+})
+radio.onReceivedString(function (receivedString) {
+    alter_Text = ""
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    Position += 1
+    if (Position == 28) {
+        Position = 0
+    }
+    Buchstabe = ABC.charAt(Position)
+    basic.showString(Buchstabe)
+})
+let alter_Text = ""
+let Buchstabe = ""
+let Text = ""
+let Position = 0
+let ABC = ""
+ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!?123"
+Position = 0
+radio.setGroup(10)
